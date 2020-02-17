005e80a22827e24ccc0aedc0e24b3e8db8152ad6
Warinyourself <dmitriysuvorov1997@gmail.com>
2020-02-17 23:18:36 +0300

    --amend

--- gh-push.sh
+++ gh-push.sh
@@ -2,4 +2,8 @@ npm run build
 
 cp -r ./dist/* ../gh-page/
 
-git add -A && git commit -am 'Update build' && git push origin gh-page
+git add -A
+
+git commit -am 'Update build'
+
+git push origin gh-pages
